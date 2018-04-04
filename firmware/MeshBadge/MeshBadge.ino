e02c35f5c484039ab1a98db6560cc964bc436b16
Brandon Satrom <bsatrom@gmail.com>
2018-04-04 17:43:36 -0500

    remove wifi logic and clean up display code

--- firmware/MeshBadge/MeshBadge.ino
+++ firmware/MeshBadge/MeshBadge.ino
@@ -4,14 +4,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// Client request libraries
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
-
-// Wifi Libraries
-#include <WiFi.h>
-#include <WiFiMulti.h>
-
 // NeoPixel Includes
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -32,23 +24,16 @@
 #define TWITTERCHECKINTERVAL 360
 #define LOWVOLTAGETHRESHOLD 3.3
 
-// Wifi values
-WiFiMulti wifiMulti;
-
-const char* ssid     = "5at70m";
-const char* password = "Sarah0812!";
-
-// Rest Interface constants
-const char* tweetHost = "https://sheets.googleapis.com"; // Tweets are stored in a Google Sheet via IFTTT
-const char* apiKey = "AIzaSyBzcsBpKziCrhy68nfm6h1Wnw1G6C9S53M";
-const char* sheetId = "1pMINVucqHspaUVzp_kydLzkL9WYeM9D3TnOeb8l1ouw";
-StaticJsonBuffer<200> jsonBuffer;
-
 long lastDisplayChange = 0;
 long lastBatteryCheck = 0;
 int textIndex = 0;
 bool updateDisplayRotator = true;
 String textStrings[TEXTARRAYSIZE] = { "Brandon", "Satrom", "DevRel @", "Particle", "@Brandon", "Satrom" };
+
+// Loop control booleans
+bool displayUpdating = false;
+bool neoPixelsUpdating = false;
+bool batteryChecking = false;
 
 //LiPo Management variables
 float startVoltage = 0;
@@ -68,37 +53,26 @@ void setup() {
   // Initialize with the I2C addr 0x3C (for the 128x32)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
+  // Clear the buffer.
   display.display();
   delay(1000);
 
-  // Clear the buffer.
-  display.clearDisplay();
-  display.display();
   paintScreen();
-
-  //Connnect to Wifi
-  wifiMulti.addAP(ssid, password);
-  while (wifiMulti.run() != WL_CONNECTED) {
-    delay(500);
-  }
-  scrollText("WiFi", "Connected!", 2);
 
   // Check the Voltage of the Power Supply
   checkBattery();
 
   // Init NeoPixel display
   initNeoPixelDisplay();
-
-  // Check twitter
-  // checkTwitterMentions();
 }
 
 void loop() {
-  if (millis() - lastDisplayChange > TEXTUPDATEINTERVAL * 1000 && updateDisplayRotator) {
+  if (millis() - lastDisplayChange > TEXTUPDATEINTERVAL * 1000 && updateDisplayRotator && !displayUpdating) {
+    displayUpdating = true;
     lastDisplayChange = millis();
 
-    display.clearDisplay();
     paintScreen();
+    displayUpdating = false;
   }
 
   if (millis() - lastBatteryCheck > BATTERYCHECKINTERVAL * 1000) {
@@ -106,7 +80,9 @@ void loop() {
     checkBattery();
   }
 
-  if (! digitalRead(BUTTON_A)) {
+  if (! digitalRead(BUTTON_A) && !neoPixelsUpdating) {
+    neoPixelsUpdating = true;
+    
     clearNeoPixelDisplay();
     strip.setBrightness(25);
 
@@ -123,10 +99,16 @@ void loop() {
     theaterChaseRainbow(50);
 
     clearNeoPixelDisplay();
+
+    neoPixelsUpdating = false;
   }
 
-  if (! digitalRead(BUTTON_C)) {
+  if (! digitalRead(BUTTON_C) && !batteryChecking) {
+    batteryChecking = true;
+    
     displayBatteryCharge();
+
+    batteryChecking = false;
   }
 }
 
@@ -189,13 +171,11 @@ void paintScreen() {
 
 // Set scrolling text on the OLED screen
 void scrollText(String line1, String line2, int textSize) {
-  display.clearDisplay();
   display.setTextWrap(false);
   display.setTextSize(textSize);
 
   display.println(line1);
   display.println(line2);
-  display.setCursor(0, 0);
   display.display();
   display.startscrollleft(0x00, 0x0F);
 }
@@ -283,63 +263,4 @@ uint32_t Wheel(byte WheelPos) {
   }
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-void checkTwitterMentions() {
-  // Create a URI for the request
-  String url = tweetHost;
-  url += "/v4/spreadsheets/";
-  url += sheetId;
-  url += "/values:batchGet?dateTimeRenderOption=SERIAL_NUMBER&majorDimension=COLUMNS&ranges=D1%3AD500&valueRenderOption=UNFORMATTED_VALUE";
-  url += "&key=";
-  url += apiKey;
-
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-
-  // Use WiFiClient class to create TCP connections
-  HTTPClient http;
-  http.begin(url);
-
-  int httpCode = http.GET();
-
-  // httpCode will be negative on error
-  if (httpCode > 0) {
-    // HTTP header has been send and Server response header has been handled
-    // file found at server
-    if (httpCode == HTTP_CODE_OK) {
-      int len = http.getSize();
-      // create buffer for read
-      uint8_t buff[128] = { 0 };
-
-      //WiFiClient * stream = http.getStreamPtr();
-      
-      // read all data from server
-      //while (http.connected() && (len > 0 || len == -1)) {
-        // get available data size
-        //size_t size = stream->available();
-
-        //if (size) {
-          // read up to 128 byte
-          //int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
-          String root = http.getString();
-
-          // write it to a String
-          //Serial.write(buff, c);
-          //JsonArray& root = jsonBuffer.parseArray(buff);
-          
-          Serial.printf("[ROOT]: %s\n", root);
-          
-          //if (len > 0) {
-          //  len -= c;
-          //}
-        //} else {
-          http.end();
-        //}
-        //delay(1);
-      //}
-    }
-  }
-
-  http.end();
 }
